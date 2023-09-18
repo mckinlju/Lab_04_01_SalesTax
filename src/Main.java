eda5ad678353f9ac11f8c6041ab55fa403790c8a
mckinlju <mckinlju@mail.uc.edu>
2023-09-18 18:45:22 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        final double SALES_TAX = 0.05;
+        double purchasePrice = 25.0; // This is the simulated input for purchase price
+        double totalCost = 0;
+
+
+
+
+
+
+
+    }
+}
