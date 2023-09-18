0f90b229aa519af0bdb9e870a42d0af91be4eedd
mckinlju <mckinlju@mail.uc.edu>
2023-09-18 18:51:58 -0400

    Completed Program

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ public class Main
     {
         final double SALES_TAX = 0.05;
         double purchasePrice = 25.0; // This is the simulated input for purchase price
+        double costOfTax = 0;
         double totalCost = 0;
 
+        costOfTax = purchasePrice * SALES_TAX;
+        System.out.println("The cost of the sales tax when the purchase price is $" + purchasePrice + " = $" + costOfTax );
 
-
-
-
-
+        totalCost = purchasePrice + costOfTax;
+        System.out.println("The total cost is $" + totalCost );
 
     }
 }
